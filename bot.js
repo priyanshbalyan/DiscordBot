29219f88b3d06b614e1527c0fe63aaca153995db
Priyansh Balyan <Priyansh Balyan>
2017-06-21 14:11:26 +0530

    Added deletelogging and welcome message features. Laid groundwork for starboard feature

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ var Discordie = require('discordie');
 
 
 var Utilities = require("./modules/utilities.js");
+var Moderation = require("./modules/moderation.js");
 
 const Events = Discordie.Events;
 const discordie = new Discordie();
@@ -14,6 +15,19 @@ discordie.connect({
 	token: Key.getBotToken() //Paste your Bot Application Token here instead of Key.getBotToken()
 });
 
+var settings = {
+	"23213231231231321":{
+		deletech:null,
+		starboardch:null,
+		welcomech:null
+	},
+	"23131321312313214":{
+		deletech:null,
+		starboardch:null,
+		welcomech:null
+	}
+};
+
 //connected to discord
 discordie.Dispatcher.on(Events.GATEWAY_READY, e => {
 	console.log("connected as " + discordie.User.username);
@@ -24,6 +38,10 @@ discordie.Dispatcher.on(Events.GATEWAY_READY, e => {
 //New member joined the server
 discordie.Dispatcher.on(Events.GUILD_MEMBER_ADD, e => {
 	//e.message.channel.sendMessage('Welcome!');
+	if(settings.hasOwnProperty(e.guild.id))
+		if(settings[e.guild.id].welcomech)
+			settings[e.guild.id].welcomech.sendMessage("Welcome! "+e.member.mention+" to "+e.guild.name);
+
 });
 
 //member left the server
@@ -33,10 +51,19 @@ discordie.Dispatcher.on(Events.GUILD_MEMBER_REMOVE, e => {
 
 discordie.Dispatcher.on(Events.MESSAGE_DELETE, e => {
 	//embed(e, e.message.content, "delete");
+	if(settings.hasOwnProperty(e.message.guild.id))
+		if(settings[e.message.guild.id].deletech)
+			settings[e.message.guild.id].deletech.sendMessage(e.message.timestamp, false, fembed(e, "delete"));
 });
 
-commands = ['ping', 'rng', 'flipcoin', 'help', 'getroles', 'avatar', 'quote', 'weather', 'clean', '8ball', 'serverinfo', 'userinfo', 'emote', 'lovecalc', 'kick', 'ban'];
-desc = ['Check ping', 'Gives a random number between 1 to 100', 'Flips a coin', 'Shows this message', 'Get the roles of the mentioned user', 'Shows user\'s avatar', 'Get a quote', 'Get weather data for a location \nUsage : ``weather <Location>``', 'Cleans messages', 'Ask 8ball anything', 'Get Server Info', 'Get user info', 'Get Emote URL', 'Calculates love between people\nUsage: ``lovecalc <mention1> <mention2>``', 'Kicks a user', 'Bans a user'];
+discordie.Dispatcher.on(Events.MESSAGE_REACTION_ADD, e => {
+	if(settings.hasOwnProperty(e.message.guild.id))
+		if(settings[e.message.guild.id].starboardch)
+			settings[e.message.guild.id].starboardch.sendMessage("Emoji: "+e.emoji.name+"\nMessage: "+e.message.content+"\nUser: "+e.user.uername);
+});
+
+commands = ['ping', 'rng', 'flipcoin', 'help', 'getroles', 'avatar', 'quote', 'weather', 'clean', '8ball', 'serverinfo', 'userinfo', 'emote', 'lovecalc', 'kick', 'ban', 'say', 'set'];
+desc = ['Check ping', 'Gives a random number between 1 to 100', 'Flips a coin', 'Shows this message', 'Get the roles of the mentioned user', 'Shows user\'s avatar', 'Get a quote', 'Get weather data for a location \nUsage : ``weather <Location>``', 'Cleans messages', 'Ask 8ball anything', 'Get Server Info', 'Get user info', 'Get Emote URL', 'Calculates love between people\nUsage: ``lovecalc <mention1> <mention2>``', 'Kicks a user', 'Bans a user', 'Make the bot say something', 'Set various channels for specific features of the bot. Call this '];
 //new message on server
 discordie.Dispatcher.on(Events.MESSAGE_CREATE, e=>{
 	//console.log(e.message.author.username);     
@@ -51,7 +78,7 @@ discordie.Dispatcher.on(Events.MESSAGE_CREATE, e=>{
 	console.log(params);
 	try{
 	switch(cmd){
-		case commands[0] : e.message.channel.sendMessage('Pong!').then(m=>m.edit('Pong! ``'+Date.now()-start+'ms``'));
+		case commands[0] : e.message.channel.sendMessage('Pong!').then(m=>m.edit('Pong! ``'+(Date.now()-start)+'ms``'));
 			break;
 
 		case commands[1] : fembed(e, "Your random number is " + Math.round(Math.random()*100), " ");
@@ -153,20 +180,44 @@ discordie.Dispatcher.on(Events.MESSAGE_CREATE, e=>{
 			break;
 
 		case commands[14]:
-			if(e.message.mentions.length>0)
-				if(e.message.author.can(Discordie.Permissions.General.KICK_MEMBERS, e.message.guild))
-					e.message.mentions[0].memberOf(e.message.guild).kick().then(()=>e.message.channel.sendMessage("***"+e.message.mentions[0].username+"#"+e.message.mentions[0].discriminator+" has been kicked!***"))
-					.catch(err=>e.message.channel.sendMessage("Can't kick member.\n"+err.message));
-				else
-					console.log("user doesn't have the perms to kick");
+			Moderation.kick(e, Discordie);
 			break;
 
 		case commands[15]:
-			if(e.message.mentions.length>0)
-				if(e.message.author.can(Discordie.Permissions.General.BAN_MEMBERS, e.message.guild))
-					e.message.guild.ban(e.message.mentions[0], 1).then(()=>e.message.channel.sendMessage("***"+e.message.mentions[0].username+"#"+e.message.mentions[0].discriminator+" has been banned!***")).catch(err=>e.message.channel.sendMessage("Can't ban member.\n"+err.message));
-				else
-					console.log("user doesn't have the perm to ban");
+			Moderation.ban(e, Discordie);
+			break;
+
+		case commands[16]:
+			e.message.channel.sendMessage(params.join(" "));
+			break;
+
+		case commands[17]://set
+			if(!settings.hasOwnProperty(e.message.guild.id)) settings[e.message.guild.id] = { welcomech:null, starboardch:null, deletech:null};
+			if(e.message.author.can(Discordie.Permissions.General.MANAGE_CHANNELS, e.message.guild))
+				switch(params[0]){
+					case "deletelog": settings[e.message.guild.id].deletech = e.message.channel;
+						e.message.channel.sendMessage("Deleted messages logging channel successfully set.");
+						break;
+
+					case "starboard": settings[e.message.guild.id].starboardch = e.message.channel;
+						e.message.channel.sendMessage("Starboard channel successfully set.");
+						break;
+
+					case "welcome": settings[e.message.guild.id].welcomech = e.message.channel;
+						e.message.channel.sendMessage("Welcome channel successfully set.");
+						break;
+
+					case "reset" :  settings[e.message.guild.id].deletech = null;
+									settings[e.message.guild.id].welcomech = null;
+									settings[e.message.guild.id].starboardch = null;
+						e.message.channel.sendMessage("Settings reset.");
+						break;
+
+					default: e.message.channel.sendMessage("``set welcome`` Sets the channel for welcome greetings when new members are added.\n``set deletelog`` Sets channel for deleted messages logging.\n``set starboard`` Sets the channel for starboard feature of the bot. Add a star reaction to a message to make the bot post it in starboard channel.\n``set reset`` Resets the guild settings.");
+						break;
+
+				}
+
 			break;
 	}
 	
@@ -174,20 +225,19 @@ discordie.Dispatcher.on(Events.MESSAGE_CREATE, e=>{
 });
 
 
-
-function fembed(e, content, event){
+function fembed(e, event){
 	const data = {
-  		"description": content,
+  		"description": e.message.content,
   		"color": 123134,
   		"author": {
     	"name": e.message.author.username,
-    	"url": "https://discordapp.com",
+    	"url": e.message.author.avatarURL,
     	"icon_url": e.message.author.avatarURL
         }
 	};
 	
 	if(event == "delete")
-		e.message.channel.sendMessage(e.message.author.username+" deleted their message", false, data);
+		return data;
 	else
 		e.message.channel.sendMessage(" ",false,data);
 }
