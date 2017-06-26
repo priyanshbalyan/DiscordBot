61f2ca79eba65805aa74d2c6bbb8c49b9a12fd97
Priyansh Balyan <Priyansh Balyan>
2017-06-26 18:43:53 +0530

    Fixed features for attachment messages

--- modules/setter.js
+++ modules/setter.js
@@ -8,15 +8,15 @@ module.exports = {
 				settings[e.message.guild.id] = { welcomech:null, starboardch:null, deletech:null };
 		if(e.message.author.can(Discordie.Permissions.General.MANAGE_CHANNELS, e.message.guild))
 			switch(params[0]){
-				case "deletelog": settings[e.message.guild.id].deletech = e.message.channel;
+				case "deletelog": settings[e.message.guild.id].deletech = e.message.channel.id;
 					e.message.channel.sendMessage("Deleted messages logging successfully set in <#"+e.message.channel.id+">.");
 					break;
 
-				case "starboard": settings[e.message.guild.id].starboardch = e.message.channel;
+				case "starboard": settings[e.message.guild.id].starboardch = e.message.channel.id;
 					e.message.channel.sendMessage("Starboard successfully set in <#"+e.message.channel.id+">.");
 					break;
 
-				case "welcome": settings[e.message.guild.id].welcomech = e.message.channel;
+				case "welcome": settings[e.message.guild.id].welcomech = e.message.channel.id;
 					e.message.channel.sendMessage("Welcome messages successfully set in <#"+e.message.channel.id+">.");
 					break;
 
@@ -32,7 +32,7 @@ module.exports = {
 						"fields":[
 							{"name":'set welcome', "value":"Sets the channel for welcome greetings when new members are added."},
 							{"name":'set deletelog', "value":"Sets the channel for deleted messages logging."},
-							{"name":'set starboard', "value":"Sets the channel for starboard feature of the bot. Add a star reaction to a message to make the bot post it in starboard channel."},
+							{"name":'set starboard', "value":"Sets the channel for starboard feature of the bot. Add a star reaction to a message to make the bot post it in starboard channel. (Requires 2 stars for the bot to post messages.)"},
 							{"name":'set reset', "value":"Resets the guild settings."},
 						]
 					};
@@ -47,21 +47,26 @@ module.exports = {
 		});
 	},
 
-	welcome:function(e, settings){
-		if(settings.hasOwnProperty(e.guild.id) && settings[e.guild.id].welcomech)
-			settings[e.guild.id].welcomech.sendMessage("Welcome! "+e.member.mention+" to "+e.guild.name);
+	welcome:function(e, discordie, settings){
+		if(settings.hasOwnProperty(e.guild.id) && settings[e.guild.id].welcomech){
+			var channel = discordie.Channels.get(settings[e.guild.id].welcomech);
+			channel.sendMessage("Welcome! "+e.member.mention+" to "+e.guild.name);
+		}
 	},
 
-	deletelog:function(e, settings){
-		if(e.message && settings.hasOwnProperty(e.message.guild.id) && settings[e.message.guild.id].deletech)
-			settings[e.message.guild.id].deletech.sendMessage(" ", false, Utilities.fembed(e, "delete"));
+	deletelog:function(e, discordie, settings){
+		if(e.message && settings.hasOwnProperty(e.message.guild.id) && settings[e.message.guild.id].deletech){
+			var channel = discordie.Channels.get(settings[e.message.guild.id].deletech);
+			channel.sendMessage(" ", false, Utilities.fembed(e, "delete"));
+		}
 	},
 
-	starboard:function(e, settings){
+	starboard:function(e, discordie, settings){
 		if(e.message && settings.hasOwnProperty(e.message.guild.id) && settings[e.message.guild.id].starboardch){
 			console.log(JSON.stringify(e.message.reactions))
 			if(e.message.reactions[0].emoji.name == "⭐" && currentstarred != e.message.id && e.message.reactions[0].count > 1){
-					e.message.channel.sendMessage(":star: in <#"+e.message.channel.id+">",false, Utilities.fembed(e, "starred"));
+					var channel = discordie.Channels.get(settings[e.message.guild.id].starboardch)
+					channel.sendMessage(":star: in <#"+e.message.channel.id+">",false, Utilities.fembed(e, "starred"));
 					currentstarred = e.message.id;
 			}
 		}
