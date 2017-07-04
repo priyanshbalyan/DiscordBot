17d95eeb25768eda337fdfb66407490e9ac23f63
Priyansh Balyan <Priyansh Balyan>
2017-07-04 20:30:35 +0530

    Fixed some stuff

--- modules/utilities.js
+++ modules/utilities.js
@@ -55,6 +55,7 @@ module.exports = {
 	eightball: function(){
 		return ballreplies[Math.round(Math.random()*ballreplies.length)];
 	},
+	
 	guildinfo: function(guild, discordie){
 		var embed = {
 				"color": 123134,
@@ -312,14 +313,14 @@ module.exports = {
 
 	googlesearch:function(e, params){
 		var url = "https://www.googleapis.com/customsearch/v1?key=AIzaSyCaGnVEJoO7JJLaIafX5t1dYYeRjSED8tw&cx=017477852080590256610:hyzdrv6behg&q="+params.join(" ");
-		request(options, (err,res,body) => {
+		request(url, (err,res,body) => {
 			if(!err){
 				var resp = JSON.parse(body);
 				//console.log(resp);
-				var str = resp.items[0].title+"\n"+resp.items[0].article.articlebody+"\n"+resp.items[0].link
-						+"\n\n**See also:**\n"+resp.items[1].link+"\n"+resp.items[2].link;
+				var str = resp.items[0].title+"\n"+resp.items[0].link
+						+"\n\n**See also:**\n<"+resp.items[1].link+">\n<"+resp.items[2].link+">";
 				
-			}else var str = "description": "Can't fetch data from google.";
+			}else var str = "Can't fetch data from google.";
 			
 			e.message.channel.sendMessage(str);
 		});
