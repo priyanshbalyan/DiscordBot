8fbabd26444a353b9a63beb1c4e30a1b0f783281
Priyansh Balyan <Priyansh Balyan>
2017-07-04 22:17:33 +0530

    fixed unmute command and purge commands

--- modules/moderation.js
+++ modules/moderation.js
@@ -1,3 +1,4 @@
+var muteTimeout;
 
 module.exports = {
 	kick:function(e, discordie){
@@ -58,9 +59,22 @@ module.exports = {
 
 	unmute:function(e, params){
 		if(!e.message.author.can(2|4|16|32|268435456, e.message.guild)) return;
-		role = e.message.guild.roles.find(r=>r.name=="Muted");
-		e.message.mentions[0].memberOf(e.message.guild).unassignRole(role);
-		e.message.channel.sendMessage(params[0]+" has been unmuted.");
+		role = e.message.member.roles.find(r=>r.name=="Muted");
+		if(role){
+			e.message.mentions[0].memberOf(e.message.guild).unassignRole(role);
+			e.message.channel.sendMessage(params[0]+" has been unmuted.");
+		}else e.message.channel.sendMessage("The user isn't muted.");
 		clearTimeout(muteTimeout);
+	},
+
+	purge:function(e, params, discordie){
+		if(!e.message.author.can(8192, e.message.guild)) return;
+		if(!discordie.User.can(8192, e.message.guild)) { e.message.channel.sendMessage("I don't have the permissions to purge messages. ``Required Perm: MANAGE_MESSAGES``"); return; }
+		if(!params[0] || parseInt(params[0])==NaN) { e.message.channel.sendMessage("You need to provide a number of messages to purge."); return;}
+		e.message.channel.fetchMessages().then(obj => {
+			let msgarray = obj.messages;
+			msgarray.length = parseInt(params[0]);
+			msgarray.map(m => m.delete().catch(console.error));
+		}).catch(console.error);
 	}
 };
