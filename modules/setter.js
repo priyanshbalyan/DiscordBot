7f3483a309e80cd705113d06de530ca8e8e42619
Priyansh Balyan <priyanshbalyan@gmail.com>
2017-08-08 13:32:08 +0530

    Fixed delete log function to log non-bot messages only

--- modules/setter.js
+++ modules/setter.js
@@ -1,44 +1,70 @@
 const fs = require('fs');
-const Utilities = require('./utilities.js');
 var currentstarred = "";
 
-function enotif(e, flag, str){
-	color = flag==1 ? 123134 : 0xe91e63;
-	var embed = {
-		"color":color,
-		"description":str
-	};
-	e.message.channel.sendMessage(" ", false, embed).then().catch(console.error);
+function enotif(e, flag, str) {
+    color = flag == 1 ? 123134 : 0xe91e63;
+    var embed = {
+        "color": color,
+        "description": str
+    };
+    e.message.channel.sendMessage(" ", false, embed).then().catch(console.error);
+}
+
+function fembed(e, str) {
+    var data = {
+        "description": e.message.content,
+        "color": 123134,
+        "author": {
+            "name": e.message.author.username,
+            "url": e.message.author.avatarURL,
+            "icon_url": e.message.author.avatarURL
+        },
+        "footer": {
+            "text": "Deleted"
+        },
+        "timestamp": e.message.timestamp
+    };
+    //console.log(e.message.attachments[0]);
+    if (e.message.attachments.length > 0)
+        if (str == "delete") data.description = e.message.content + "(Deleted attachment)";
+        else data["image"] = { "url": e.message.attachments[0].url };
+
+    if (e.message.embeds.length > 0) return e.message.embeds[0];
+
+    if (str == "delete") { data.footer.text = "Deleted"; return data; }
+    if (str == "starred") { data.footer.text = "Starred"; return data; }
+
+    e.message.channel.sendMessage(" ", false, { "description": str });
 }
 
 module.exports = {
 
-	welcome:function(e, discordie, settings){
-		if(settings.hasOwnProperty(e.guild.id) && settings[e.guild.id].welcomech){
-			var channel = discordie.Channels.get(settings[e.guild.id].welcomech);
-			channel.sendMessage(e.member.mention+"Welcome "+" to "+e.guild.name +" !");
-		}
-	},
+    welcome: function(e, discordie, settings) {
+        if (settings.hasOwnProperty(e.guild.id) && settings[e.guild.id].welcomech) {
+            var channel = discordie.Channels.get(settings[e.guild.id].welcomech);
+            channel.sendMessage(e.member.mention + "Welcome " + " to " + e.guild.name + " !");
+        }
+    },
 
-	deletelog:function(e, discordie, settings){
-		//if guild has set delete channel
-		if(e.message && settings.hasOwnProperty(e.message.guild.id) && settings[e.message.guild.id].deletech){
-			var channel = discordie.Channels.get(settings[e.message.guild.id].deletech);
-			channel.sendMessage(" ", false, Utilities.fembed(e, "delete"));
-		}
-	},
+    deletelog: function(e, discordie, settings) {
+        //if guild has set delete channel
+        if (e.message && settings.hasOwnProperty(e.message.guild.id) && settings[e.message.guild.id].deletech && e.message.author.bot == false) {
+            var channel = discordie.Channels.get(settings[e.message.guild.id].deletech);
+            channel.sendMessage(" ", false, fembed(e, "delete"));
+        }
+    },
 
-	starboard:function(e, discordie, settings){
-		//if starboard is set in the guild
-		if(e.message && settings.hasOwnProperty(e.message.guild.id) && settings[e.message.guild.id].starboardch){
-			console.log(JSON.stringify(e.message.reactions))
-			//if the reaction is a star
-			if(e.message.reactions[0].emoji.name.charCodeAt() == 11088 && currentstarred != e.message.id && e.message.reactions[0].count > 0){
-					var channel = discordie.Channels.get(settings[e.message.guild.id].starboardch)
-					channel.sendMessage(":star: in <#"+e.message.channel.id+"> ID:"+e.message.id,false, Utilities.fembed(e, "starred"));
-					currentstarred = e.message.id;
-			}
-		}
-	}
+    starboard: function(e, discordie, settings) {
+        //if starboard is set in the guild
+        if (e.message && settings.hasOwnProperty(e.message.guild.id) && settings[e.message.guild.id].starboardch) {
+            console.log(JSON.stringify(e.message.reactions))
+                //if the reaction is a star
+            if (e.emoji.name.charCodeAt() == 11088 && currentstarred != e.message.id && e.message.reactions[0].count > 0) {
+                var channel = discordie.Channels.get(settings[e.message.guild.id].starboardch)
+                channel.sendMessage(":star: in <#" + e.message.channel.id + "> ID:" + e.message.id, false, fembed(e, "starred"));
+                currentstarred = e.message.id;
+            }
+        }
+    }
 
 };
