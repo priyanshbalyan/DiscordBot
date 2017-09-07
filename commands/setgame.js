19e361086ebf1b59b8113612615d09cf2a482d0a
Priyansh Balyan <priyanshbalyan@gmail.com>
2017-09-07 16:40:41 +0530

    Fixed broken command due to discord api changes

--- commands/setgame.js
+++ commands/setgame.js
@@ -1,5 +1,6 @@
 exports.run = (e, params, discordie) => {
-	if(e.message.author.id !== "279207740340043776") return;
-	var game = {"name": params.join(" ")};
-	discordie.User.setGame(game);
+    if (e.message.author.id !== "279207740340043776") return;
+    var game = { type:0, "name": params.join(" ") };
+    discordie.User.setGame(null, game);
+    e.message.channel.sendMessage('Set playing status to ' + params.join(' '));
 }
