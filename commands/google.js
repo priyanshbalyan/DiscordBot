beb88f2442be586e23903b23e796c3b2e005d0a9
Priyansh Balyan <priyanshbalyan@gmail.com>
2017-09-07 16:39:17 +0530

    Config changes

--- commands/google.js
+++ commands/google.js
@@ -1,15 +1,18 @@
-var request = require('request');
+let request = require('request');
+const Config = require('../config.json');
 
 exports.run = (e, params, discordie) => {
 	if(!params || params.length <1) return e.message.channel.sendMessage("No search term provided.");
-	var url = "https://www.googleapis.com/customsearch/v1?key=AIzaSyCaGnVEJoO7JJLaIafX5t1dYYeRjSED8tw&cx=017477852080590256610:hyzdrv6behg&q="+params.join("+");
+	let url = "https://www.googleapis.com/customsearch/v1?key="+Config.GOOGLE_SEARCH_API_KEY+"&cx=017477852080590256610:hyzdrv6behg&q="+params.join("+");
 	request(url, (err,res,body) => {
 	if(!err){
-		var resp = JSON.parse(body);
+		//console.log(body);
+		let resp = JSON.parse(body);
 		//console.log(resp);
-		var str = resp.items[0].title+"\n"+resp.items[0].link
+		str = resp.items[0].title+"\n"+resp.items[0].link
 				+"\n\n**See also:**\n<"+resp.items[1].link+">\n<"+resp.items[2].link+">";		
-	}else var str = "Can't fetch data from google.";	
-		e.message.channel.sendMessage(str);
+	}else 
+		str = "Can't fetch data from google.";	
+	e.message.channel.sendMessage(str);
 	});
 }
