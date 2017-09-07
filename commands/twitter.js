315739ad15e6c2fe0fd879a83d0557ad85fc7c30
Priyansh Balyan <priyanshbalyan@gmail.com>
2017-09-07 16:38:10 +0530

    Config changes

--- commands/twitter.js
+++ commands/twitter.js
@@ -1,41 +1,78 @@
 const fs = require('fs');
-var Utilities = require('../modules/utilities.js')
+var Twitter = require('twitter');
+const Config = require('../config.json');
+
+var client = new Twitter({
+    consumer_key: Config.TWITTER_CONSUMER_KEY,
+    consumer_secret: Config.TWITTER_CONSUMER_SECRET,
+    access_token_key: Config.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: Config.TWITTER_ACCESS_TOKEN_SECRET
+});
+var feed;
+
+function TwitterFeed(user, ch, discordie) {
+    this.user = user;
+    this.stream = client.stream('user', user);
+    this.stream.on('data', ev = function(event) {
+        console.log(event);
+        console.log('\n\nscreen: ' + event.user.screen_name + "  user: " + user);
+        if (event.user.screen_name == user) {
+            discordie.Channels.get(ch).sendMessage("**New tweet from @" + user + "**\n" + event.text);
+        }
+    });
+
+    this.stream.on('error', er = function(error) {
+        console.log('error' + error);
+    });
+
+    TwitterFeed.prototype.endstream = function() {
+        this.stream.removeListener('data', ev);
+        this.stream.removeListener('error', er);
+    }
+}
 
 exports.run = (e, params, discordie) => {
-	let settings = JSON.parse(fs.readFileSync('./settings.json','utf8'));
+    //let settings = JSON.parse(fs.readFileSync('./settings.json', 'utf8'));
 
-	switch(params[0]){
-		case 'watch':
-			handlename = params[1];
-			if(!settings.hasOwnProperty("twitter")) settings.twitter = [];
-			if(handlename.startsWith('@')){
-				handlename = handlename.split('')
-				handlename.shift();
-				handlename = handlename.join('');
-			}
-			settings.twitter.push({"handle":handlename, "channel":e.message.channel.id});
-			e.message.channel.sendMessage("Watching tweets from **@"+handlename+"** user.");
-			Utilities.twitterService(discordie, settings);
-			break;
+    switch (params[0]) {
+        case 'watch':
+            handlename = params[1];
+            //if (!settings.hasOwnProperty("twitter")) settings.twitter = [];
+            if (handlename.startsWith('@')) {
+                handlename = handlename.split('')
+                handlename.shift();
+                handlename = handlename.join('');
+            }
 
-		case 'stop' :
-			if(settings.twitter.find(h=>h.channel == e.message.channel.id))
-				settings.twitter.splice(settings.twitter.indexOf(settings.twitter.find(h=>h.channel==e.message.channel.id)),1);
-			e.message.channel.sendMessage("Stopped watching tweets.");
-			break;
+            if (feed) {
+                feed.endstream();
+                delete feed;
+            }
+            feed = new TwitterFeed(handlename, e.message.channel, discordie);
+            e.message.channel.sendMessage("Watching tweets from **@" + handlename + "** user.");
+            break;
 
-		default:
-			var embed = {
-				"color":123134,
-				"fields":[
-					{"name":"twitter watch ``<username>``", "value":"Watches and posts tweets from the mentioned twitter username"},
-					{"name":"twitter stop", "value":"Stops watching the twitter feed"}
-				]
-			};
-			e.message.channel.sendMessage(" ",false,embed);
-	}
+        case 'stop':
+            if (feed) {
+                feed.endstream();
+                delete feed;
+                e.message.channel.sendMessage("Stopped watching tweets.");
+            } else
+                e.message.channel.sendMessage("No feeds found to stop.");
+            break;
 
-	fs.writeFile("./settings.json", JSON.stringify(settings, null, 4), err => {
-		if(err) console.log(err);
-	});
+        default:
+            var embed = {
+                "color": 123134,
+                "fields": [
+                    { "name": "twitter watch ``<username>``", "value": "Watches and posts tweets from the mentioned twitter username" },
+                    { "name": "twitter stop", "value": "Stops watching the twitter feed" }
+                ]
+            };
+            e.message.channel.sendMessage(" ", false, embed);
+    }
+
+    //fs.writeFile("./settings.json", JSON.stringify(settings, null, 4), err => {
+    //    if (err) console.log(err);
+    //});
 }
