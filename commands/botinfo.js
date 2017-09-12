c6e99e36a145cebda3c604ad13304b249890f60e
Priyansh Balyan <priyanshbalyan@gmail.com>
2017-09-12 20:12:20 +0530

    Added some more info

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,4 +1,5 @@
 const moment = require('moment');
+const Config = require('./config.json');
 
 exports.run = (e, params, discordie) => {
 
@@ -9,7 +10,7 @@ exports.run = (e, params, discordie) => {
 		},
 		"description":"[Invite link](https://discordapp.com/oauth2/authorize?client_id=289776005504040960&scope=bot)",
 		"footer":{
-			"text":"Using Discordie, request, fs"
+			"text":"Version "+Config.VERSION+" | npm packages: discordie, request, fs, xml2js, easyimage"
 		},
 		"fields":[
 			{ "name": "Developer", "value":"Raiden#6960", "inline":true},
@@ -19,7 +20,7 @@ exports.run = (e, params, discordie) => {
 			{ "name": "Platform", "value":process.platform, "inline":true},
 			{ "name": "GitHub", "value":"Soon", "inline":true},
 			{ "name": "Memory Usage", "value":Math.floor(process.memoryUsage().heapUsed/1024/1024)+"MB / "+Math.floor(process.memoryUsage().heapTotal/1024/1024)+"MB Total", "inline":true},			
-			{ "name": "Uptime", "value":moment(process.uptime()*1000).from(moment(0),true), "inline":true}
+			{ "name": "Uptime", "value":moment(process.uptime()*1000).from(moment(0),true), "inline":true},
 			{ "name": "Support Server", "value":"[Link](http://discord.gg/URyEVcs)"}
 		]
 	};	
