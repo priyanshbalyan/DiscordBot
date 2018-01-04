8da31c3966f5f84a6f992435632860d70dac8a40
Priyansh Balyan <priyanshbalyan@gmail.com>
2018-01-04 15:36:16 +0530

    Fixed bug where eval command doesnt work for other prefixes

--- commands/eval.js
+++ commands/eval.js
@@ -1,3 +1,5 @@
+const Config = require('../config.json');
+
 function cleancode(text){
 	if(typeof(text) === "string")
 		return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g,"@"+String.fromCharCode(8203));
@@ -5,16 +7,17 @@ function cleancode(text){
 }
 
 exports.run = (e, params, discordie) => {
-	if(e.message.author.id !== "279207740340043776") return;
-	try{
-		const code = e.message.content.replace("]eval ","");
-		let evaled = eval(code);
+	if(e.message.author.id !== Config.SELF_ID) return;
 
+	try{
+		const code = e.message.content.replace(Config.DEFAULT_PREFIX+"eval ","");
+		//console.log(code);
+		let evaled = eval(code);
 		if(typeof(evaled) !== "string")
 			evaled = require("util").inspect(evaled);
 
-		e.message.channel.sendMessage("```js\n"+cleancode(evaled)+"\n```", {code: "xl"});
+		e.message.channel.sendMessage("```js\n"+cleancode(evaled)+"\n```",false);
 	}catch(err){
-		e.message.channel.sendMessage("```xl\n"+cleancode(err)+"\n```");
+		e.message.channel.sendMessage("```xl\n"+cleancode(err)+"\n```",false);
 	}
 }
