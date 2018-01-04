10b092a8c19d19860f5121d84fa1cb8a2b4681ec
Priyansh Balyan <priyanshbalyan@gmail.com>
2018-01-04 15:42:22 +0530

    Fixed prefix token to be looked up from config file

--- selfbot.js
+++ selfbot.js
@@ -2,51 +2,61 @@ const request = require('request');
 const Discordie = require('discordie');
 const fs = require('fs');
 
+const Config = require('./config.json');
 const Setter = require("./modules/setter.js");
 
 const Events = Discordie.Events;
 const discordie = new Discordie({ autoReconnect: true });
 
-var prefix = ";";
+let prefix = ';' //Config.DEFAULT_PREFIX;
 let settings = JSON.parse(fs.readFileSync('./settings.json'));
 
+
 discordie.connect({
-    token: require('./key.js').getToken(), //Paste your Bot Application Token here instead of Key.getBotToken()
+    token: Config.SELF_TOKEN, //Paste your Bot Application Token here instead of Key.getBotToken()
 });
 
 //connected to discord
 discordie.Dispatcher.on(Events.GATEWAY_READY, e => {
     console.log("connected as " + discordie.User.username);
-    var game = { type: 1, name: prefix + "help", url: "https://goo.gl/ezgx4t" };
+    var game = { type: 0, name: prefix + "help", url: "https://goo.gl/ezgx4t" };
     discordie.User.setGame(null, game);
 
 });
 
-//New member joined the server
-discordie.Dispatcher.on(Events.GUILD_MEMBER_ADD, e => {
-    //e.message.channel.sendMessage('Welcome!');
-    Setter.welcome(e, discordie, settings);
-});
+// //New member joined the server
+// discordie.Dispatcher.on(Events.GUILD_MEMBER_ADD, e => {
+//     //e.message.channel.sendMessage('Welcome!');
+//     Setter.welcome(e, discordie, settings);
+// });
 
-//member left the server
-discordie.Dispatcher.on(Events.GUILD_MEMBER_REMOVE, e => {
-    //e.message.channel.sendMessage('A User has left this channel.');
-});
+// //member left the server
+// discordie.Dispatcher.on(Events.GUILD_MEMBER_REMOVE, e => {
+//     //e.message.channel.sendMessage('A User has left this channel.');
+//     let settings = JSON.parse(fs.readFileSync('./settings.json','utf8'));
+//     Setter.goodbye(e, discordie, settings);
+// });
 
-discordie.Dispatcher.on(Events.MESSAGE_DELETE, e => {
-    //embed(e, e.message.content, "delete");
-    Setter.deletelog(e, discordie, settings)
-});
+// discordie.Dispatcher.on(Events.MESSAGE_DELETE, e => {
+//     //embed(e, e.message.content, "delete");
+//     let settings = JSON.parse(fs.readFileSync('./settings.json','utf8'));
+//     Setter.deletelog(e, discordie, settings)
+// });
 
-discordie.Dispatcher.on(Events.MESSAGE_REACTION_ADD, e => {
-    Setter.starboard(e, discordie, settings);
-});
+// discordie.Dispatcher.on(Events.MESSAGE_REACTION_ADD, e => {
+//     let settings = JSON.parse(fs.readFileSync('./settings.json','utf8'));
+//     Setter.starboard(e, discordie, settings);
+// });
 
 //new message on server
 discordie.Dispatcher.on(Events.MESSAGE_CREATE, e => {
     //console.log(e.message.author.username);  
-    if(e.message.author.id !== "279207740340043776") return;
-    if (!e.message.content.startsWith(prefix)) return;
+    if(e.message.author.id !== Config.SELF_ID) return;
+        if (e.message.isPrivate) {
+           
+        } else {
+
+            if (!e.message.content.startsWith(prefix)) return;
 
             const params = e.message.content.split(/\n|\s/).slice(1); //split at space and new line
             var cmd = e.message.content.split(' ')[0].split("");
@@ -63,6 +73,5 @@ discordie.Dispatcher.on(Events.MESSAGE_CREATE, e => {
                 }
             }
             //console.log(params);
-        
-    
+        }
 });
