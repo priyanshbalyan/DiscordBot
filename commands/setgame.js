a400f56bfbdf67c5ebe71164dba183cce37336ed
Priyansh Balyan <priyanshbalyan@gmail.com>
2018-01-04 15:41:01 +0530

    Fixed userid to be looked up in config file

--- commands/setgame.js
+++ commands/setgame.js
@@ -1,5 +1,7 @@
+const Config = require('../config.json');
+
 exports.run = (e, params, discordie) => {
-    if (e.message.author.id !== "279207740340043776") return;
+    if (e.message.author.id !== Config.SELF_ID) return;
     var game = { type:0, "name": params.join(" ") };
     discordie.User.setGame(null, game);
     e.message.channel.sendMessage('Set playing status to ' + params.join(' '));
