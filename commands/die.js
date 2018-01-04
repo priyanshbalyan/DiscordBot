81085e4f7501a3a1c6e3a19a2a79a0f78fb0bb08
Priyansh Balyan <priyanshbalyan@gmail.com>
2018-01-04 15:37:42 +0530

    Fixed userid to be obtained from config file

--- commands/die.js
+++ commands/die.js
@@ -1,5 +1,7 @@
+const Config = require('../config.json');
+
 exports.run = (e, params, discordie) => {
-	if(e.message.author.id !== "279207740340043776") return;
+	if(e.message.author.id !== Config.SELF_ID) return;
 	console.log("Disconnecting...");
 	discordie.disconnect();
 }
