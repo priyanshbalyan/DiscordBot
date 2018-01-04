cb7d63797d8be3ff565481cc95a9bc570c498cad
Priyansh Balyan <priyanshbalyan@gmail.com>
2018-01-04 15:41:09 +0530

    Fixed userid to be looked up in config file

--- commands/setusername.js
+++ commands/setusername.js
@@ -1,4 +1,7 @@
+const Config = require('../config.json');
+
 exports.run = (e, params, discordie) => {
-	if(e.message.author.id !== "279207740340043776") return;
-	discordie.User.setUsername(params.join(" "));
+    if (e.message.author.id !== Config.SELF_ID) return;
+    discordie.User.setUsername(params.join(" "));
+    e.message.channel.sendMessage('Username set to ' + params.join(' '));
 }
